277b59bd7b2724a0c9c3128aca8f524d27fe8e00
Fabricio Adenir da Silva <fabricio.adenir@softplan.com.br>
2020-05-20 13:51:21 -0300

    Ajustes

--- system/settings.py
+++ system/settings.py
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -121,5 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#Routines execute
+# Routines execute
 EXECUTE_ROUTINE = False
